48a380b54d104edf436caac5e33dd56fe0336301
Jin <ajhz011@aucklanduni.ac.nz>
2024-12-31 16:24:28 +1300

    Properly set up the firebase db

--- ecosphere-goods/firebase.js
+++ ecosphere-goods/firebase.js
@@ -1,12 +1,22 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
 require('dotenv').config();
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: "ecosphere-goods.firebaseapp.com",
-    projectId: "ecosphere-goods",
-    storageBucket: "ecosphere-goods.firebasestorage.app",
-    messagingSenderId: "308765407455",
-    appId: "1:308765407455:web:1769e1bf4967d35b64d16a",
-    measurementId: "G-VPEEB7F5VF"
-  };
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "ecosphere-goods.firebaseapp.com",
+  projectId: "ecosphere-goods",
+  storageBucket: "ecosphere-goods.firebasestorage.app",
+  messagingSenderId: "308765407455",
+  appId: "1:308765407455:web:1769e1bf4967d35b64d16a",
+  measurementId: "G-VPEEB7F5VF"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { auth }
+export default db
