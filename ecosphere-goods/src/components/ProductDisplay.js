eb934e8bcce7e5142197f99afa1da24345b04da5
Jin <ajhz011@aucklanduni.ac.nz>
2025-01-02 16:01:30 +1300

    Updated ProductDisplay component to grab product data from firebase instead of the json array

--- ecosphere-goods/src/components/ProductDisplay.js
+++ ecosphere-goods/src/components/ProductDisplay.js
@@ -1,17 +1,55 @@
-import React from 'react'
-import heroSliderData from '../data/heroSliderData.json'
+import React, { useState, useEffect } from 'react'
+import { collection, query, where, getDocs } from '../firebase';
 import Item from './Item'
+import db from '../firebase'
 
 const ProductDisplay = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const productsRef = collection(db, 'products')
+            const q = query(productsRef, where('active', '==', true))
+            const querySnapShot = await getDocs(q);
+            const products = {};
+
+            const productPromises = querySnapShot.docs.map(async (productDoc) => {
+                const productData = productDoc.data();
+                const priceSnap = await getDocs(collection(productDoc.ref, 'prices'));
+        
+                const prices = priceSnap.docs.map((price) => ({
+                  priceId: price.id,
+                  priceData: price.data(),
+                }));
+        
+                productData.prices = prices;
+                products[productDoc.id] = productData;
+              });
+        
+              await Promise.all(productPromises);
+              setProducts(products);
+        }
+
+        fetchProducts()
+    }, [])
+
+    useEffect(() => {
+        Object.entries(products).forEach(([productId, productData]) => {
+            console.log(`Product Prices: ${productData.prices[0].priceData.unit_amount}`);
+        });
+    }, [products]);
+
+    console.log(products)
+
+
     return (
         <div className='flex flex-wrap overflow-y-scroll justify-between h-full'>
-            {heroSliderData.map((item, index) => (
-                <div className='w-80 mb-8 pr-8'>
-                    <Item 
-                        key={ index } 
-                        imageUrl={ item.url } 
-                        price='69' 
-                        name='Lorem Ipsum'
+            {Object.entries(products).map(([productId, productData]) => (
+                <div className='w-80 mb-8 pr-8' key={ productId } >
+                    <Item  
+                        imageUrl={ productData.images[0] } 
+                        price={ productData.prices[0].priceData.unit_amount / 100 } 
+                        name={ productData.name }
                     />
                 </div>
             ))}
