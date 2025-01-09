8aef14ca9b4dc62e18928ea248061ac1241191ec
Jin <ajhz011@aucklanduni.ac.nz>
2025-01-09 15:32:00 +1300

    Adjusted the FinalPricingInformation div to accomodate the ToastContainer without it taking up too much space

--- ecosphere-goods/src/components/ShoppingCart/FinalPricingInformation.js
+++ ecosphere-goods/src/components/ShoppingCart/FinalPricingInformation.js
@@ -13,8 +13,6 @@ const FinalPricingInformation = () => {
     const cartItems = useSelector(selectCart);
     const user = useSelector(selectUser)
 
-    console.log('USER', user)
-
     const loadCheckout = async () => {
         let customerDocRef;
         if (user) {
@@ -58,36 +56,39 @@ const FinalPricingInformation = () => {
     
 
     return (
-        <div className='flex flex-col space justify-between h-full'>
+        <div className='h-full'>
+            <div className='flex flex-col space justify-between h-full'>
+                <div className='flex flex-col space-y-16'>
+                <div className='border-3 w-9/12 border-dark-brown'/>
+
+                    <div className='font-header text-dark-brown tracking-widest'>
+                        <div className='flex space-x-8 items-center'>
+                            <p className='text-subtitle opacity-80'>SUBTOTAL:</p>
+                            <p className='text-sHeader'>{unitToDollarString(subTotal)}</p>
+                        </div>
+                        <div className='flex space-x-8 items-center'>
+                            <p className='text-body opacity-80'>SHIPPING:</p>
+                            <p className='text-subtitle'>$5.00</p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className='flex flex-col space-y-12 border-t-2 border-dark-brown border-opacity-60 pt-3'>
+                    <div className='flex font-header text-dark-brown tracking-widest items-center space-x-8'>
+                        <p className='opacity-80 text-subtitle'>CART TOTAL:</p>
+                        <p className='text-header'>{unitToDollarString(subTotal + 500)}</p>
+                    </div>
+
+                    <button 
+                        className='p-3 pl-6 pr-6 rounded-full bg-dark-brown text-off-white tracking-3px hover:scale-105 transition-transform ease-in-out duration-300'
+                        onClick={loadCheckout}
+                    >
+                        CHECKOUT
+                    </button>
+                </div>
+            </div>
+
             <ToastContainer/>
-            <div className='flex flex-col space-y-16 mt-4'>
-               <div className='border-3 w-9/12 border-dark-brown'/>
-
-                <div className='font-header text-dark-brown tracking-widest'>
-                    <div className='flex space-x-8 items-center'>
-                        <p className='text-subtitle opacity-80'>SUBTOTAL:</p>
-                        <p className='text-sHeader'>{unitToDollarString(subTotal)}</p>
-                    </div>
-                    <div className='flex space-x-8 items-center'>
-                        <p className='text-body opacity-80'>SHIPPING:</p>
-                        <p className='text-subtitle'>$5.00</p>
-                    </div>
-                </div>
-            </div>
-            
-            <div className='flex flex-col space-y-12 border-t-2 border-dark-brown border-opacity-60 pt-3'>
-                <div className='flex font-header text-dark-brown tracking-widest items-center space-x-8'>
-                    <p className='opacity-80 text-subtitle'>CART TOTAL:</p>
-                    <p className='text-header'>{unitToDollarString(subTotal + 500)}</p>
-                </div>
-
-                <button 
-                    className='p-3 pl-6 pr-6 rounded-full bg-dark-brown text-off-white tracking-3px hover:scale-105 transition-transform ease-in-out duration-300'
-                    onClick={loadCheckout}
-                >
-                    CHECKOUT
-                </button>
-            </div>
         </div>
     )
 }
