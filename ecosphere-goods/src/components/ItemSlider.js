d39b6218fdd8354c900d0851a61156c4df5e73c3
Jin <ajhz011@aucklanduni.ac.nz>
2025-01-12 11:07:28 +1300

    Added a proper loader to the ItemSlider

--- ecosphere-goods/src/components/ItemSlider.js
+++ ecosphere-goods/src/components/ItemSlider.js
@@ -7,9 +7,11 @@ import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import Item from './Item';
 import './ItemSlider.css'
 import db, { collection, query, getDocs, orderBy, limit, where } from '../firebase';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const ItemSlider = ({ nextButtonClass, prevButtonClass, sortBy }) => {
   const [products, setProducts] = useState([])
+  const buttonStyle = 'cursor-pointer w-8 h-8 opacity-50 hover:opacity-100 text-dark-brown transition-opacity ease-in-out duration-2000'
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -41,50 +43,51 @@ const ItemSlider = ({ nextButtonClass, prevButtonClass, sortBy }) => {
 
     fetchProducts()
   }, [sortBy])
-
-  console.log('SORT BY: ', sortBy, products)
   
   return (
     <div className='p-4'>
       {products.length > 0 ? (
-        <Swiper
-        slidesPerView={4}
-        spaceBetween={30}
-        centeredSlides={true}
-        loop={true}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={{
-          nextEl: `.${nextButtonClass}`,
-          prevEl: `.${prevButtonClass}`,
-        }}
-        modules={[Pagination, Navigation]}
-        className="mySwiper"
-      >
-        {Object.entries(products).map(([productId, productData]) => (
-          <SwiperSlide key={productId} className="flex-shrink-0 w-72 p-6">
-            <Item 
-              imageUrl={ productData.images[0] } 
-              price={ productData.prices[0].priceData.unit_amount } 
-              name={ productData.name }
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      ) : (
-        <div> LOADING NOW </div>
-      )}
-      
+        <div>
+          <Swiper
+            slidesPerView={4}
+            spaceBetween={30}
+            centeredSlides={true}
+            loop={true}
+            pagination={{
+              clickable: true,
+            }}
+            navigation={{
+              nextEl: `.${nextButtonClass}`,
+              prevEl: `.${prevButtonClass}`,
+            }}
+            modules={[Pagination, Navigation]}
+            className="mySwiper"
+          >
+            {Object.entries(products).map(([productId, productData]) => (
+              <SwiperSlide key={productId} className="flex-shrink-0 w-72 p-6">
+                <Item 
+                  imageUrl={ productData.images[0] } 
+                  price={ productData.prices[0].priceData.unit_amount } 
+                  name={ productData.name }
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
 
-      <div className="flex relative justify-between align-middle top-1/2 space-x-6 p-8">
-          <FaChevronLeft 
-              className={`${prevButtonClass} cursor-pointer w-8 h-8 opacity-50 hover:opacity-100 text-dark-brown transition-opacity ease-in-out duration-2000`}
-          />
-          <FaChevronRight
-              className={`${nextButtonClass} cursor-pointer w-8 h-8 opacity-50 hover:opacity-100 text-dark-brown transition-opacity ease-in-out duration-2000`}
-          />
-      </div>
+          <div className="flex relative justify-between align-middle top-1/2 space-x-6 p-8">
+            <FaChevronLeft 
+                className={`${prevButtonClass} ${buttonStyle}`}
+            />
+            <FaChevronRight
+                className={`${nextButtonClass} ${buttonStyle}`}
+            />
+          </div>
+        </div>
+      ) : (
+        <div className='h-full w-full flex items-center justify-center'>
+          <CircularProgress size={64} color="secondary" sx={{ color: '#362D2D' }}/>
+        </div>
+      )}
     </div>
   )
 }
