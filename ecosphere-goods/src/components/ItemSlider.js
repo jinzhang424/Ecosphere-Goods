346c5df36a44165340daa0713d4774d297b6055b
Jin <ajhz011@aucklanduni.ac.nz>
2025-01-12 10:48:41 +1300

    Updated ItemSlider to also fetch products from the database

--- ecosphere-goods/src/components/ItemSlider.js
+++ ecosphere-goods/src/components/ItemSlider.js
@@ -1,48 +1,91 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { Pagination, Navigation } from 'swiper/modules';
-import heroSliderData from '../data/heroSliderData.json';
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import Item from './Item';
 import './ItemSlider.css'
+import db, { collection, query, getDocs, orderBy, limit, where } from '../firebase';
 
-const ItemSlider = ({ nextButtonClass, prevButtonClass }) => {
+const ItemSlider = ({ nextButtonClass, prevButtonClass, sortBy }) => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const productsRef = collection(db, 'products')
+        const q = query(productsRef, where('active', '==', true), orderBy(sortBy, 'asc'), limit(7))
+        const querySnapShot = await getDocs(q)
+        const products = [];
+
+        const productPromises = querySnapShot.docs.map(async (productDoc) => {
+          const productData = productDoc.data();
+          const priceSnap = await getDocs(collection(productDoc.ref, 'prices'))
+
+          const prices = priceSnap.docs.map((price) => ({
+            priceId: price.id,
+            priceData: price.data(),
+          }));
+
+          productData.prices = prices;
+          products.push({ id: productDoc.id, ...productData});
+        })
+
+        await Promise.all(productPromises)
+        setProducts(products)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchProducts()
+  }, [sortBy])
+
+  console.log('SORT BY: ', sortBy, products)
   
   return (
-      <div className='p-4'>
+    <div className='p-4'>
+      {products.length > 0 ? (
         <Swiper
-          slidesPerView={4}
-          spaceBetween={30}
-          centeredSlides={true}
-          loop={true}
-          pagination={{
-            clickable: true,
-          }}
-          navigation={{
-            nextEl: `.${nextButtonClass}`,
-            prevEl: `.${prevButtonClass}`,
-          }}
-          modules={[Pagination, Navigation]}
-          className="mySwiper"
-        >
-          {heroSliderData.map((item, index) => (
-            <SwiperSlide key={index} className="flex-shrink-0 w-72">
-              <Item imageUrl={item.url} price="9.00" name="Lorem Ipsum"></Item>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        slidesPerView={4}
+        spaceBetween={30}
+        centeredSlides={true}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={{
+          nextEl: `.${nextButtonClass}`,
+          prevEl: `.${prevButtonClass}`,
+        }}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {Object.entries(products).map(([productId, productData]) => (
+          <SwiperSlide key={productId} className="flex-shrink-0 w-72 p-6">
+            <Item 
+              imageUrl={ productData.images[0] } 
+              price={ productData.prices[0].priceData.unit_amount } 
+              name={ productData.name }
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      ) : (
+        <div> LOADING NOW </div>
+      )}
+      
 
-        <div className="flex relative justify-between align-middle top-1/2 space-x-6 p-8">
-            <FaChevronLeft 
-                className={`${prevButtonClass} cursor-pointer w-8 h-8 opacity-50 hover:opacity-100 text-dark-brown transition-opacity ease-in-out duration-2000`}
-            />
-            <FaChevronRight
-                className={`${nextButtonClass} cursor-pointer w-8 h-8 opacity-50 hover:opacity-100 text-dark-brown transition-opacity ease-in-out duration-2000`}
-            />
-        </div>
+      <div className="flex relative justify-between align-middle top-1/2 space-x-6 p-8">
+          <FaChevronLeft 
+              className={`${prevButtonClass} cursor-pointer w-8 h-8 opacity-50 hover:opacity-100 text-dark-brown transition-opacity ease-in-out duration-2000`}
+          />
+          <FaChevronRight
+              className={`${nextButtonClass} cursor-pointer w-8 h-8 opacity-50 hover:opacity-100 text-dark-brown transition-opacity ease-in-out duration-2000`}
+          />
       </div>
+    </div>
   )
 }
 
--- ecosphere-goods/src/components/SpecialItems.js
+++ ecosphere-goods/src/components/SpecialItems.js
@@ -6,13 +6,13 @@ const SpecialItems = () => {
         <div className='bg-off-white'>
             <div className='p-7'>
                 <h1 className='text-header font-header'>Trending Items</h1>
-                <ItemSlider nextButtonClass="trending-next" prevButtonClass="trending-prev" />
+                <ItemSlider nextButtonClass="trending-next" prevButtonClass="trending-prev" sortBy='times_sold' />
             </div>
             <div className="border-b-2 border-dark-brown pb-2 w-11/12 mx-auto"></div>
             <br />
             <div className='p-7 mt-2'>
                 <h1 className='text-header font-header'>Latest Collection</h1>
-                <ItemSlider nextButtonClass="latest-next" prevButtonClass="latest-prev" />
+                <ItemSlider nextButtonClass="latest-next" prevButtonClass="latest-prev" sortBy='date_created' />
             </div>
         </div>
     )
