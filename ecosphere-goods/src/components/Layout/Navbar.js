6b95b9d8a4401bbd073ba0af21f631c0fb56f1ea
Jin <ajhz011@aucklanduni.ac.nz>
2025-01-12 11:32:26 +1300

    Added some styling to the Navbar to further help the user identify which page they're on and made the logo properly direct you to the home page

--- ecosphere-goods/src/components/Layout/Navbar.js
+++ ecosphere-goods/src/components/Layout/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { IoMdSearch } from "react-icons/io";
 import { IoCartOutline } from "react-icons/io5";
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import Logo from '../utility/Logo';
 import { useState, useEffect } from 'react';
 import { MdAccountCircle } from "react-icons/md";
@@ -9,6 +9,7 @@ import { MdAccountCircle } from "react-icons/md";
 
 const Navbar = () => {
   const [show, handleShow] = useState(true);
+  const navLinkStyle = ({ isActive }) => isActive ? 'text-off-white bg-dark-brown p-2 pl-3 pr-3 rounded-md' : 'hover:bg-dark-brown hover:text-off-white transition-colors ease-in-out duration-300 p-2 pl-3 pr-3 rounded-md'
   
   const transitionNavBar = () => {
     if (window.scrollY > 250) {
@@ -25,26 +26,37 @@ const Navbar = () => {
 
   return (
     <div className={`fixed top-0 left-0 right-0 z-10 flex justify-between items-center bg-off-white p-8 w-full text-dark-brown transition-opacity ease-in-out duration-200 ${show ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
-      <div className="ml-4">
+      <Link className="ml-4" to="/">
         <Logo/>
-      </div>
+      </Link>
 
       <div className="flex items-center space-x-8 text-subtitle mr-4 transition-">
         <NavLink 
           to="/"
-          className="hover:underline duration-300 ease-in-out">
+          className={navLinkStyle}>
             Home
         </NavLink>
 
         <NavLink 
           to="/products" 
-          className="hover:underline duration-300 ease-in-out">
+          className={navLinkStyle}>
             Products
         </NavLink>
 
-        <NavLink className="hover:underline duration-300 ease-in-out">About Us</NavLink>
-        <NavLink to='/shopping-cart'><IoCartOutline className="w-7 h-7 cursor-pointer"/></NavLink>
-        <NavLink to='/user-portal'><MdAccountCircle className="w-7 h-7 cursor-pointer"/></NavLink>
+        <NavLink 
+          to='/about-us' 
+          className={navLinkStyle}>
+            About Us
+        </NavLink>
+
+        <NavLink to='/shopping-cart'>
+            <IoCartOutline className="w-7 h-7 cursor-pointer"/>
+        </NavLink>
+        
+        <NavLink to='/user-portal'>
+          <MdAccountCircle className="w-7 h-7 cursor-pointer"/>
+        </NavLink>
+        
         <IoMdSearch className="w-7 h-7 cursor-pointer"/>
       </div>
     </div>
