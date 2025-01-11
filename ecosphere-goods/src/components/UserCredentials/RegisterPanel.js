512d4653677274fb60c3749306677f6f4998d8e6
Jin <ajhz011@aucklanduni.ac.nz>
2025-01-12 12:33:31 +1300

    Added the BackToBrowsingButton to the sign in and register panel

--- ecosphere-goods/src/components/UserCredentials/RegisterPanel.js
+++ ecosphere-goods/src/components/UserCredentials/RegisterPanel.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import RegisterEmail from './RegisterEmail'
+import BackToBrowsingButton from '../utility/BackToBrowsingButton'
 
 const RegisterPanel = ({ backToSignIn }) => {
     return (
         <div className={`w-full flex-shrink-0 space-y-8 p-32`}>
+            <BackToBrowsingButton/>
             <h1 className='font-LHeader text-header'>Register</h1>
             <RegisterEmail backToSignIn={ backToSignIn }/>
         </div>
