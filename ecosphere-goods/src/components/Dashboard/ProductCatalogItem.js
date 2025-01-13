ffc519eac9ecbd25adf35805bc8c593909cef70b
Jin <ajhz011@aucklanduni.ac.nz>
2025-01-14 10:45:43 +1300

    Created the basic layout for ProductCatalogItem

--- ecosphere-goods/src/components/Dashboard/ProductCatalogItem.js
+++ ecosphere-goods/src/components/Dashboard/ProductCatalogItem.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import unitToDollarString from '../../utilityFunctions/unitToDollarString'
 
 const ProductCatalogItem = (productData) => {
+    const productImgUrl = productData.productData.images[0]
+    const productName = productData.productData.name
+    const productUnitAmount = productData.productData.prices[0].priceData.unit_amount
+    const productCategory = productData.productData.stripe_metadata_itemCategory
+
     return (
-        <div>
-            { productData.productData.name }
-            { productData.productData.prices[0].priceData.unit_amount }
-        </div>
+        <span className='flex justify-between text-dark-brown font-header h-24'>
+            <img 
+                src={ productImgUrl } 
+                className='aspect-square object-cover' 
+            />
+            
+            <p>{ productName }</p>
+            <p>{ productCategory }</p>
+            <p>{ unitToDollarString(productUnitAmount) }</p>
+        </span>
     )
 }
 
