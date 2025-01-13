9e53f4cd2dbda4208ae4f88b6ccac4e04b5833d5
Jin <ajhz011@aucklanduni.ac.nz>
2025-01-13 18:23:52 +1300

    Wrapped the navigation icons in a NavLink and made it so that when its active, it has a background to indicate it to be so

---
+++ ecosphere-goods/src/components/Dashboard/AdminProductCatalog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminProductCatalog = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AdminProductCatalog
--- ecosphere-goods/src/components/Dashboard/DashBoardNav.js
+++ ecosphere-goods/src/components/Dashboard/DashBoardNav.js
@@ -4,19 +4,29 @@ import { GoChecklist } from "react-icons/go";
 import { ImExit } from "react-icons/im";
 import { FaCog } from "react-icons/fa";
 import { BsBoxes } from "react-icons/bs";
+import { NavLink } from 'react-router-dom';
 
 const DashBoardNav = () => {
     const iconSize = 'w-10 h-10'
     const isAdmin = true;
+    const navLinkStyle = ''
 
     return (
         <div className='flex flex-col items-center justify-between h-full p-6 bg-off-white rounded-3xl pt-10 pb-10'>
-            <div className='space-y-8'>
-                <IoIosHome className={ iconSize }/>
-                { isAdmin ? <BsBoxes className={iconSize} /> : <GoChecklist className={ iconSize }/>}
-                
-                <FaCog className={ iconSize }/>
+            <div className='flex flex-col space-y-8'>
+                <NavLink className={({ isActive }) => isActive && 'bg-dark-brown text-off-white rounded-xl' } to='dashboard/home'>
+                    <IoIosHome className={ iconSize }/>
+                </NavLink>
+
+                <NavLink className={({ isActive }) => isActive && 'bg-dark-brown text-off-white rounded-xl' } to={`dashboard/${ isAdmin ? 'product-catalog' : 'past-orders'}`}>
+                    { isAdmin ? <BsBoxes className={iconSize} /> : <GoChecklist className={ iconSize }/>}
+                </NavLink>
+
+                <NavLink className={({ isActive }) => isActive && 'bg-dark-brown text-off-white rounded-xl' } to='dashboard/settings'>
+                    <FaCog className={ iconSize }/>
+                </NavLink>
             </div>
+            
             <div className='pt-7 border-t-2 border-dark-brown'>   
                 <ImExit className={ iconSize }/>
             </div>
