bc42121edbb28fcf3a3fcef87c09d002f2ea2497
Jin <ajhz011@aucklanduni.ac.nz>
2025-01-19 13:58:46 +1300

    Adjusted the fetching of products to call the backend

---
+++ ecosphere-goods/src/utilityFunctions/productHandling.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const fetchProducts = async (filters, minUnitCost, maxUnitCost, order) => {
+    try {
+        const response = await axios.get('http://localhost:5000/products/fetch-products', {
+            params: {
+                minUnitCost,
+                maxUnitCost,
+                filters,
+                order
+            },
+        });
+
+        return response.data.data
+    } catch (error) {
+        console.error('Error details:', error.response || error.message || error);
+        throw new Error(error.response?.data?.message || 'Error occured while fetching products')
+    }
+}
