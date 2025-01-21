f7b7b00325d710d8aa503cb83b52b1835e25f397
Jin <ajhz011@aucklanduni.ac.nz>
2025-01-21 13:57:23 +1300

    Added the route for deleting a product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const { fetchProducts, addNewProduct } = require('../controllers/productController')
+const { fetchProducts, addNewProduct, deleteProduct } = require('../controllers/productController')
 
 const router = express.Router()
 
 router.get('/fetch-products', fetchProducts)
 router.post('/add-new-product', addNewProduct)
+
+router.delete('/delete-product', deleteProduct)
 
 module.exports = router
