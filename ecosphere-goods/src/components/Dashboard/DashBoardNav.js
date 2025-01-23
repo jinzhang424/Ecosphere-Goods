a3b373a270d6829755252817942f60cf184679ee
Jin <ajhz011@aucklanduni.ac.nz>
2025-01-24 10:22:59 +1300

    Made it so DashBoardNav now fetches the user role instead of using the temporary preset value

--- ecosphere-goods/src/components/Dashboard/DashBoardNav.js
+++ ecosphere-goods/src/components/Dashboard/DashBoardNav.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { IoIosHome } from "react-icons/io";
 import { GoChecklist } from "react-icons/go";
 import { ImExit } from "react-icons/im";
 import { FaCog } from "react-icons/fa";
 import { BsBoxes } from "react-icons/bs";
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../features/userSlice';
+import { fetchRole } from '../../utilityFunctions/userAuth'
 
 const DashBoardNav = () => {
+    const user = useSelector(selectUser)
+    const [userRole, setUserRole] = useState('')
     const iconSize = 'w-10 h-10'
-    const isAdmin = true;
     const activeNavLinkStyle = 'bg-light-brown text-off-white rounded-lg p-1'
     const inactiveNavLinkStyle = 'hover:bg-light-brown hover:text-off-white rounded-lg transition-colors ease-in-out duration-500 p-1 text-dark-brown'
+
+    useEffect(() => {
+        const fetchUserRole = () => {
+            const role = fetchRole(user.uid)
+            setUserRole(role)
+        }
+
+        fetchUserRole()
+    }, [user])
 
     return (
         <div className='flex flex-col items-center justify-between h-full p-5 bg-off-white rounded-3xl pt-10 pb-10'>
@@ -19,8 +32,8 @@ const DashBoardNav = () => {
                     <IoIosHome className={ iconSize }/>
                 </NavLink>
 
-                <NavLink className={({ isActive }) => isActive ? activeNavLinkStyle : inactiveNavLinkStyle } to={`/dashboard/${ isAdmin ? 'admin/product-catalog' : 'past-orders'}`}>
-                    { isAdmin ? <BsBoxes className={iconSize} /> : <GoChecklist className={ iconSize }/>}
+                <NavLink className={({ isActive }) => isActive ? activeNavLinkStyle : inactiveNavLinkStyle } to={`/dashboard/${ userRole == 'admin' ? 'admin/product-catalog' : 'past-orders'}`}>
+                    { userRole == 'admin' ? <BsBoxes className={iconSize} /> : <GoChecklist className={ iconSize }/>}
                 </NavLink>
 
                 <NavLink className={({ isActive }) => isActive ? activeNavLinkStyle : inactiveNavLinkStyle } to='/dashboard/settings'>
--- ecosphere-goods/src/layouts/DashBoardLayout.js
+++ ecosphere-goods/src/layouts/DashBoardLayout.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import DashBoardNav from '../components/Dashboard/DashBoardNav'
-import { Outlet, useLocation } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 
 const DashBoardLayout = () => {
-  const currentURL = useLocation()
 
   return (
     <div className='flex space-x-8 h-screen p-8 bg-light-brown'>
