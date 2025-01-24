65ce18d5a07d93c63b21e1455bb26b242c4c1e28
Jin <ajhz011@aucklanduni.ac.nz>
2025-01-25 12:25:46 +1300

    Wrapped the DashBoardLayout with OrderProvider

--- ecosphere-goods/src/layouts/DashBoardLayout.js
+++ ecosphere-goods/src/layouts/DashBoardLayout.js
@@ -2,15 +2,18 @@ import React from 'react'
 import DashBoardNav from '../components/Dashboard/DashBoardNav'
 import { Outlet } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
+import { OrderProvider } from '../components/Dashboard/Order/OrderContext'
 
 const DashBoardLayout = () => {
 
   return (
-    <div className='flex space-x-8 h-screen p-8 bg-light-brown'>
-      <DashBoardNav/>
-      <main className='w-full'><Outlet/></main>
-      <ToastContainer/>
-    </div>
+    <OrderProvider>
+      <div className='flex space-x-8 h-screen p-8 bg-light-brown'>
+        <DashBoardNav/>
+        <main className='w-full'><Outlet/></main>
+        <ToastContainer/>
+      </div>
+    </OrderProvider>
   )
 }
 
