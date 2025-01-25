a8bc49ea5ecbfbdcf9d0592ac80eb194ffe7a1da
Jin <ajhz011@aucklanduni.ac.nz>
2025-01-25 13:56:49 +1300

    Renamed OrderItems to OrderDisplay and added a loader to Orders

--- ecosphere-goods/src/components/Dashboard/Order/OrderDisplay.js
+++ ecosphere-goods/src/components/Dashboard/Order/OrderDisplay.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ProductCatalogItem from '../ProductCatalogItem'
 
-const OrderItems = ({ order }) => {
+const OrderDisplay = ({ order }) => {
   return (
     <div className='bg-light-brown opacity-10 h-72 rounded-3xl p-8'>
       <p className='text-dark-brown'>
-        {order.orders[0]?.orderData.products[0].product.name}
+        { order.customer_email }
       </p>
     </div>
   )
 }
 
-export default OrderItems
+export default OrderDisplay
--- ecosphere-goods/src/components/Dashboard/Order/Orders.js
+++ ecosphere-goods/src/components/Dashboard/Order/Orders.js
@@ -8,13 +8,16 @@ import { OrderContext } from './OrderContext'
 
 const Orders = () => {
   const { orders, setOrders } = useContext(OrderContext)
+  const [ isLoading, setIsLoading ] = useState(false)
   const user = useSelector(selectUser)
 
   useEffect(() => {
     const getOrders = async () => {
+      setIsLoading(true)
       try {
         const orders = await fetchOrders(user.uid)
         setOrders(orders)
+        setIsLoading(false)
       } catch (error) {
         console.log(error.message)
         toast.error('Error occurred while fetching products.')
@@ -24,14 +27,18 @@ const Orders = () => {
     getOrders()
 
   }, [user.uid])
-  
-  console.log(orders)
 
   return (
     <div className='bg-off-white rounded-3xl w-full h-full p-8'>
       <h1 className='font-header text-header text-dark-brown'>Orders</h1>
       <div className='h-full overflow-hidden'>
-        
+        {isLoading ? (
+          <div>Is Loading</div>
+        ) : (
+          orders.map((order, orderID) => (
+            <OrderDisplay key={orderID} order={order}/>
+          ))
+        )}
       </div>
     </div>
   )
