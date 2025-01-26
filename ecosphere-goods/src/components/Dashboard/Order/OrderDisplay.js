df880a0ecfd7f7dbcf1a030da60c9bc772b46bf7
Jin <ajhz011@aucklanduni.ac.nz>
2025-01-26 13:19:55 +1300

    Changed the customer id thats used in the view button path to properly use the order's customer's id

--- ecosphere-goods/src/components/Dashboard/Order/OrderDisplay.js
+++ ecosphere-goods/src/components/Dashboard/Order/OrderDisplay.js
@@ -4,13 +4,10 @@ import { HiShoppingBag } from "react-icons/hi2";
 import { FaCircle } from "react-icons/fa";
 import unitToDollarString from '../../../utilityFunctions/unitToDollarString';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { selectUser } from '../../../features/userSlice';
 
 const OrderDisplay = ({ order }) => {
-  const user = useSelector(selectUser)
 
-  console.log(order)
+  console.log(order.customer_id)
   return (
     <div className='bg-light-brown bg-opacity-15 h-fit rounded-3xl p-6'>
       <span className='flex items-center justify-between pb-4 border-b-2 border-dark-brown border-opacity-10'>
@@ -35,7 +32,7 @@ const OrderDisplay = ({ order }) => {
         <p className='font-header text-dark-brown'>Total: {unitToDollarString(order.orderData.total_price)}</p>
         <Link 
           className='font-header p-2 pl-5 pr-5 bg-dark-brown bg-opacity-0 text-dark-brown rounded-full border-2 border-dark-brown hover:bg-opacity-100 hover:text-off-white transition-all ease-in-out duration-300'
-          to={`${user.uid}/${order.orderID}`}
+          to={`${order.customer_id}/${order.orderID}`}
         >
             View Order
         </Link>
