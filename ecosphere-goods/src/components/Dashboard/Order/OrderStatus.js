6139c47d45825d64f2622a7adfcfce1d944d290b
Jin <ajhz011@aucklanduni.ac.nz>
2025-01-27 15:13:18 +1300

    Made it so that if the order is not pending then the status will be red

--- ecosphere-goods/src/components/Dashboard/Order/OrderStatus.js
+++ ecosphere-goods/src/components/Dashboard/Order/OrderStatus.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { FaCircle } from "react-icons/fa";
 
 const OrderStatus = ({ orderStatus }) => {
+  const isPending = orderStatus === 'Pending'
+
   return (
     <div className='flex items-center space-x-3 p-2 pl-4 pr-4 rounded-full bg-orange-300 bg-opacity-30'>
-        <FaCircle className='text-orange-500'/>
-        <p className='font-header text-orange-500'>{orderStatus}</p>
+        <FaCircle className={`${isPending ? 'text-orange-500' : 'text-red-600'}`}/>
+        <p className={`font-header ${isPending ? 'text-orange-500' : 'text-red-600'}`}>{orderStatus}</p>
     </div>
   )
 }
