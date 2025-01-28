da04c561f675edaee7def00f2298f61ad40b54cf
Jin <ajhz011@aucklanduni.ac.nz>
2025-01-28 13:37:43 +1300

    Updated DhashBoardHomePage to properly render the customer or admin dashboard based on user role

--- ecosphere-goods/src/pages/DashBoardHomePage.js
+++ ecosphere-goods/src/pages/DashBoardHomePage.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import UserDashBoard from '../components/Dashboard/UserDashBoard'
 import AdminDashBoard from '../components/Dashboard/AdminDashBoard'
+import { useSelector } from 'react-redux'
+import { selectUser } from '../features/userSlice'
 
 const DashBoardHomePage = () => {
-  const isAdmin = true;
+  const user = useSelector(selectUser)
 
   return (
     <div className='w-full h-full'>
-      { isAdmin ? <AdminDashBoard/> : <UserDashBoard/> }
+      { user.role === 'admin' ? <AdminDashBoard/> : <UserDashBoard/> }
     </div>
   )
 }
