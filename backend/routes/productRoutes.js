cea7dc73713539667928bf877ee1304d0d4b8aaf
Jin <ajhz011@aucklanduni.ac.nz>
2025-01-30 12:03:17 +1300

    IGNORE THIS. This shouldve been in the last commit

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const { fetchProducts, fetchProductByName, addNewProduct, deleteProduct, updateProduct } = require('../controllers/productController')
+const { fetchProducts, fetchProductById, addNewProduct, deleteProduct, updateProduct } = require('../controllers/productController')
 
 const router = express.Router()
 
 router.get('/fetch-products', fetchProducts)
 router.post('/add-new-product', addNewProduct)
-router.get('/fetch-product-by-name', fetchProductByName)
+router.get('/fetch-product-by-id', fetchProductById)
 router.put('/update-product', updateProduct)
 router.delete('/delete-product', deleteProduct)
 
