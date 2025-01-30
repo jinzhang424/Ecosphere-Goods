cc2c2f58a57e243c96aa5d1ee25f38c9d339af59
Jin <ajhz011@aucklanduni.ac.nz>
2025-01-31 12:26:34 +1300

    Adjusted TruckLoader to slide out when loading is finished

--- ecosphere-goods/src/components/animations/TruckLoader.js
+++ ecosphere-goods/src/components/animations/TruckLoader.js
@@ -2,12 +2,15 @@ import React from 'react'
 import truck from '../../data/images/truck.gif'
 import Logo from '../utility/Logo'
 
-const TruckLoader = () => {
+const TruckLoader = ({ isLoading }) => {
     return (
-        <div className='absolute inset-0 bg-off-white z-40 flex flex-col'>
-            <div className='relative bottom-10 m-auto flex-col items-center'>
-                <img src={truck} alt="" className='w-36 m-auto'/>
-                <Logo />
+        <div>
+            <div className={`absolute inset-0 bg-dark-brown z-30 ${isLoading ? '' : '-translate-y-full'} transition-transform ease-in duration-500`}></div>
+            <div className={`absolute inset-0 bg-off-white z-40 flex flex-col ${isLoading ? '' : '-translate-y-full'} transition-transform ease-in duration-300`}>
+                <div className='relative bottom-10 m-auto flex-col items-center'>
+                    <img src={truck} alt="" className='w-36 m-auto'/>
+                    <Logo />
+                </div>
             </div>
         </div>
     )
