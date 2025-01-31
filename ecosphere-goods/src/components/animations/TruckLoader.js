71e871160685f54b018ab2d2f3dce300f5c380f3
Jin <ajhz011@aucklanduni.ac.nz>
2025-01-31 15:43:02 +1300

    Turned off point events for TruckLoader

--- ecosphere-goods/src/components/animations/TruckLoader.js
+++ ecosphere-goods/src/components/animations/TruckLoader.js
@@ -4,7 +4,7 @@ import Logo from '../utility/Logo'
 
 const TruckLoader = ({ isLoading }) => {
     return (
-        <div>
+        <div className='pointer-events-none'>
             <div className={`absolute inset-0 bg-dark-brown z-30 ${isLoading ? '' : '-translate-y-full'} transition-transform ease-in duration-500`}></div>
             <div className={`absolute inset-0 bg-off-white z-40 flex flex-col ${isLoading ? '' : '-translate-y-full'} transition-transform ease-in duration-300`}>
                 <div className='relative bottom-10 m-auto flex-col items-center'>
