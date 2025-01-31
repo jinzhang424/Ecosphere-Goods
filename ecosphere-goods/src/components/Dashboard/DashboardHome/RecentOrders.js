67fcb47e79a85e6ddc6b176b36c4f96c0b2ce099
Jin <ajhz011@aucklanduni.ac.nz>
2025-01-31 16:11:59 +1300

    Removed pointer events from TruckComponentLoader and added the loader to RecentOrders

--- ecosphere-goods/src/components/Dashboard/DashboardHome/RecentOrders.js
+++ ecosphere-goods/src/components/Dashboard/DashboardHome/RecentOrders.js
@@ -3,19 +3,24 @@ import { fetchOrders } from '../../../utilityFunctions/orderHandling'
 import { useSelector } from 'react-redux'
 import { selectUser } from '../../../features/userSlice'
 import RecentOrderItem from './RecentOrderItem'
+import TruckComponentLoader from '../../animations/TruckComponentLoader'
 
 const RecentOrders = () => {
     const user = useSelector(selectUser)
     const [orders, setOrders] = useState([])
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
         const fetchOrder = async () => {
+            setLoading(true)
             try {
                 const orders = await fetchOrders(user.uid)
                 const slicedOrders = orders.slice(0, 2)
                 setOrders(slicedOrders)
             } catch (error) {
                 console.log(error.message)
+            } finally {
+                setLoading(false)
             }
         }
 
@@ -28,10 +33,13 @@ const RecentOrders = () => {
         <div className='flex flex-col w-full h-full overflow-hidden'>
             <h1 className='font-header text-dark-brown text-header'>Recent Orders</h1>
 
-            <div className='flex flex-col w-full space-y-8 mt-8'>
-                {orders.map((order, index) => (
-                    <RecentOrderItem key={index} order={order}/>
-                ))}
+            <div className='relative'>
+                <TruckComponentLoader loading={loading}/>
+                <div className='relative flex flex-col w-full space-y-8 mt-8'>
+                    {orders.map((order, index) => (
+                        <RecentOrderItem key={index} order={order}/>
+                    ))}
+                </div>
             </div>
         </div>
     )
--- ecosphere-goods/src/components/animations/TruckComponentLoader.js
+++ ecosphere-goods/src/components/animations/TruckComponentLoader.js
@@ -5,7 +5,7 @@ import truck from '../../data/images/truck.gif'
 const TruckComponentLoader = ({ loading = false }) => {
 
     return (
-        <div className={`absolute object-center inset-0 z-40 h-full  ${loading ? '' : 'opacity-0'} transition-opacity ease-in duration-300 bg-off-white`}>
+        <div className={`absolute object-center inset-0 z-30 pointer-events-none h-full  ${loading ? '' : 'opacity-0'} transition-opacity ease-in duration-300 bg-off-white`}>
             <div className='relative top-10 inset-0 flex flex-col h-full'>
                 <div className='relative m-auto flex-col items-center'>
                     <img src={truck} alt="" className='w-36 m-auto'/>
