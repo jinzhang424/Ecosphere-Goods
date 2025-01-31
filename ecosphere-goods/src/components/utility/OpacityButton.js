46f89fdde17fbb6efddfb2e90cf7f654d1a9e312
Jin <ajhz011@aucklanduni.ac.nz>
2025-01-31 14:05:40 +1300

    Made it so the OpacityButton's background color is adjustable and replaced the buttons in ConfirmDeleteDialog

--- ecosphere-goods/src/components/utility/OpacityButton.js
+++ ecosphere-goods/src/components/utility/OpacityButton.js
@@ -2,19 +2,19 @@ import React, { useState } from 'react'
 import CircularProgress from '@mui/material/CircularProgress';
 
 // Changes opacity on hover
-const OpacityButton = ({ label, type='', onClick = async () => {}, loading = false }) => {
+const OpacityButton = ({ label, type='', onClick = async () => {}, loading = false, bgColor = 'bg-dark-brown'}) => {
 
-    const handleClick = async () => {
-        await onClick()
+    const handleClick = async (event) => {
+        await onClick(event)
     }
 
     return (
         <button 
             onClick={handleClick} 
             type={type} 
-            className='flex items-center align-middle justify-center text-off-white font-header bg-dark-brown p-2 pl-4 pr-4 rounded-lg hover:bg-opacity-85'
+            className={`flex items-center align-middle justify-center text-off-white font-header ${bgColor} p-2 pl-4 pr-4 rounded-lg hover:bg-opacity-85`}
         >
-                <CircularProgress color='#362D2D' thickness={6} size={18} className={`absolute w-full h-full text-off-white ${loading ? '' : 'opacity-0'}`}/>
+                <CircularProgress color={'#362D2D'} thickness={6} size={18} className={`absolute w-full h-full text-off-white ${loading ? '' : 'opacity-0'}`}/>
                 <span className={`opacity ${loading ? 'opacity-0' : ''}`}>{label}</span>
         </button>
     )
