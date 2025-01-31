9b5addf72155ebf3695ac5340a0cc0d227a9ef3a
Jin <ajhz011@aucklanduni.ac.nz>
2025-01-31 16:06:36 +1300

    Created TruckComponentLoader made specfically for components

--- ecosphere-goods/src/components/Dashboard/Order/Orders.js
+++ ecosphere-goods/src/components/Dashboard/Order/Orders.js
@@ -5,19 +5,20 @@ import { useSelector } from 'react-redux'
 import { selectUser } from '../../../features/userSlice'
 import { toast } from 'react-toastify'
 import { OrderContext } from './OrderContext'
+import TruckComponentLoader from '../../animations/TruckComponentLoader'
 
 const Orders = () => {
   const { orders, setOrders } = useContext(OrderContext)
-  const [ isLoading, setIsLoading ] = useState(false)
+  const [ loading, setLoading ] = useState(false)
   const user = useSelector(selectUser)
 
   useEffect(() => {
     const getOrders = async () => {
-      setIsLoading(true)
+      setLoading(true)
       try {
         const orders = await fetchOrders(user.uid)
         setOrders(orders)
-        setIsLoading(false)
+        setLoading(false)
       } catch (error) {
         console.log(error.message)
         toast.error('Error occurred while fetching products.')
@@ -33,16 +34,14 @@ const Orders = () => {
 
       <h1 className='font-header text-header text-dark-brown'>Orders</h1>
 
-      <div className='h-fit overflow-hidden mt-4'>
-        {isLoading ? (
-          <div>Is Loading</div>
-        ) : (
-          <div className='flex flex-col space-y-8'>
-            {orders.map((order, orderID) => (
-              <OrderDisplay key={orderID} order={order}/>
-            ))}
-          </div>
-        )}
+      <div className='relative h-fit overflow-hidden mt-4'>
+        <TruckComponentLoader loading={loading}/>
+
+        <div className='flex flex-col space-y-8'>
+          {orders.map((order, orderID) => (
+            <OrderDisplay key={orderID} order={order}/>
+          ))}
+        </div>
       </div>
     </div>
   )
