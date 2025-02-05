f87dbd06dee6e6cea12803f5568d2660fa80373a
Jin <ajhz011@aucklanduni.ac.nz>
2025-02-05 13:55:26 +1300

    Fully Refactored ShoppingProductDialog and made the button the Item component

--- ecosphere-goods/src/components/Item.js
+++ ecosphere-goods/src/components/Item.js
@@ -2,19 +2,19 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import unitToDollarString from '../utilityFunctions/unitToDollarString';
 
-const Item = ({imageUrl, price, name, productId}) => {
+const Item = ({ productData }) => {
+
+  console.log(productData)
 
   return (
     <div className="p-6 w-full justify-between bg-dark-brown aspect-itemCard cursor-pointer hover:scale-110 transition-transform ease-in-out duration-300">
-      <Link to={`${productId}`}>
-        <div className="h-4/6">
-          <img src={imageUrl} alt="" className="object-cover w-full h-full"/>
-        </div>
-        <div className="flex flex-col justify-between h-2/6">
-          <h1 className='text-off-white font-header text-subtitle mt-2'>{name}</h1>
-          <h2 className='text-off-white font-header '>{unitToDollarString(price)}</h2>
-        </div>
-      </Link>
+      <div className="h-4/6">
+        <img src={productData.images[0]} alt="" className="object-cover w-full h-full"/>
+      </div>
+      <div className="flex flex-col justify-between h-2/6">
+        <h1 className='text-off-white font-header text-subtitle mt-2'>{productData.name}</h1>
+        <h2 className='text-off-white font-header '>{unitToDollarString(productData.prices[0]?.priceData?.unit_amount)}</h2>
+      </div>
     </div>
   )
 }
--- ecosphere-goods/src/components/ShoppingProductDialog.js
+++ ecosphere-goods/src/components/ShoppingProductDialog.js
@@ -5,11 +5,13 @@ import { useDispatch } from 'react-redux';
 import { toast, ToastContainer } from 'react-toastify';
 import QuantityAdjuster from './utility/QuantityAdjuster';
 import AddToCartButton from './utility/AddToCartButton';
+import Item from './Item';
 
-const ShoppingProductDialog = ({ product }) => {
+const ShoppingProductDialog = ({ productData }) => {
     const dispatch = useDispatch()
     const [quantity, setQuantity] = useState(1)
     const fillerText = 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum '
+    const [open, setOpen] = useState(false)
 
     const handleIncrement = () => {
         setQuantity(quantity + 1)
@@ -23,7 +25,7 @@ const ShoppingProductDialog = ({ product }) => {
 
     const handleAddToCart = () => {
         try {
-            dispatch(addItemBulk({ product: product, quantity: quantity }));
+            dispatch(addItemBulk({ product: productData, quantity: quantity }));
             toast.success('Successfully Added to Cart');
         } catch (error) {
             toast.error('Error, something has gone wrong.');
@@ -32,35 +34,45 @@ const ShoppingProductDialog = ({ product }) => {
         }
     }
 
+    const handleOpen = () => setOpen(true)
+    const handleClose = () => setOpen(false)
+
     return (
-        <div className='w-full'>
-            <div className='flex p-20 space-x-48 bg-off-white rounded-3xl'>
-                <img 
-                    className='w-5/12 object-cover object-center h-5/6 rounded-3xl aspect-1/1 border-4 border-dark-brown border-opacity-50'
-                    src={ product.images[0] } 
-                    alt="" 
-                />
-                <div className='flex flex-col space-y-12'>
-                    <article className='flex flex-col space-y-4'>
-                        <h1 className='font-LHeader text-header text-dark-brown underline underline-offset-4'>{ product.name }</h1>
-                        <p>{ product.description != null ? product.description : fillerText }</p>
-                    </article>
-                    
-                    <div className='flex justify-between items-center'>
-                        <h2 className='font-LHeader text-sHeader text-dark-brown'>{ unitToDollarString(product.prices[0].priceData.unit_amount) }</h2>
-                        <QuantityAdjuster 
-                            increment={ handleIncrement } 
-                            decrement={ handleDecrement } 
-                            quantity={ quantity } 
-                            className='rounded-md bg-dark-brown overflow-hidden bg-opacity-30 h-fit'
-                        />
-                        <AddToCartButton onClick={handleAddToCart}>
-                            Add To Cart
-                        </AddToCartButton>
+        <div>
+            <button onClick={handleOpen}>
+                <Item productData={productData}/>
+            </button>
+
+            <div className={`opacity-0 ${open ? 'opacity-100' : 'pointer-events-none'} transition-opacity ease-in-out duration-300`}>
+                <div className='fixed inset-0 bg-black bg-opacity-50 z-10' onClick={handleClose}/>
+                <dialog open className='fixed inset-0 w-2/3 flex p-20 space-x-48 bg-off-white rounded-3xl z-10'>
+                    <img 
+                        className='w-5/12 object-cover object-center h-5/6 rounded-3xl aspect-1/1 border-4 border-dark-brown border-opacity-50'
+                        src={ productData.images[0] } 
+                        alt="" 
+                    />
+                    <div className='flex flex-col space-y-12'>
+                        <article className='flex flex-col space-y-4'>
+                            <h1 className='font-LHeader text-header text-dark-brown underline underline-offset-4'>{ productData.name }</h1>
+                            <p>{ productData.description != null ? productData.description : fillerText }</p>
+                        </article>
+                        
+                        <div className='flex justify-between items-center'>
+                            <h2 className='font-LHeader text-sHeader text-dark-brown'>{ unitToDollarString(productData.prices[0].priceData.unit_amount) }</h2>
+                            <QuantityAdjuster 
+                                increment={ handleIncrement } 
+                                decrement={ handleDecrement } 
+                                quantity={ quantity } 
+                                className='rounded-md bg-dark-brown overflow-hidden bg-opacity-30 h-fit'
+                            />
+                            <AddToCartButton onClick={handleAddToCart}>
+                                Add To Cart
+                            </AddToCartButton>
+                        </div>
                     </div>
-                </div>
+                    <ToastContainer/>
+                </dialog>
             </div>
-            <ToastContainer/>
         </div>
     )
 }
