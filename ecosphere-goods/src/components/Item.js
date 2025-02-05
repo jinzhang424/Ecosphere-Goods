d7282ddc323d0c277b53f036e5ac7698d88e3f65
Jin <ajhz011@aucklanduni.ac.nz>
2025-02-06 10:19:06 +1300

    Adjusted the style of item

--- ecosphere-goods/src/components/Item.js
+++ ecosphere-goods/src/components/Item.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import unitToDollarString from '../utilityFunctions/unitToDollarString';
+import { FaCartPlus } from "react-icons/fa";
 
 const Item = ({ productData }) => {
 
-  console.log(productData)
+  const price = unitToDollarString(productData.prices[0]?.priceData?.unit_amount)
 
   return (
-    <div className="p-6 w-full justify-between bg-dark-brown aspect-itemCard cursor-pointer hover:scale-110 transition-transform ease-in-out duration-300">
+    <div className="p-6 w-full justify-between bg-dark-brown aspect-itemCard cursor-pointer hover:scale-110 transition-transform ease-in-out duration-300 rounded-xl">
       <div className="h-4/6">
-        <img src={productData.images[0]} alt="" className="object-cover w-full h-full"/>
+        <img src={productData.images[0]} alt="" className="object-cover w-full h-full object-center rounded-t-xl"/>
       </div>
-      <div className="flex flex-col justify-between h-2/6">
-        <h1 className='text-off-white font-header text-subtitle mt-2'>{productData.name}</h1>
-        <h2 className='text-off-white font-header '>{unitToDollarString(productData.prices[0]?.priceData?.unit_amount)}</h2>
-      </div>
+      
+      <span className="flex flex-col text-left justify-between h-2/6 text-off-white">
+        <h1 className='font-header text-subtitle mt-2'>{productData.name}</h1>
+
+        <h2 className='font-header '>{price}</h2>
+      </span>
     </div>
   )
 }
