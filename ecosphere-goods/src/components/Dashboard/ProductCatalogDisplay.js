a9f49fa45b20443a9f678184bb8e0b04b6bc2739
Jin <ajhz011@aucklanduni.ac.nz>
2025-02-08 11:42:34 +1300

    Adjusted styling and layout of the DashBoardLayout and product catalog

--- ecosphere-goods/src/components/Dashboard/ProductCatalogDisplay.js
+++ ecosphere-goods/src/components/Dashboard/ProductCatalogDisplay.js
@@ -14,74 +14,80 @@ const ProductCatalogDisplay = () => {
     const [productsOnPage, setProductsOnPage] = useState([])
     const [curPage, setCurPage] = useState(1)
     const [loading, setLoading] = useState(true)
-    const productsPerPage = 8;
+    const productsPerPage = 7;
 
     console.log(products)
 
     useEffect(() => {
-        const pages = Math.ceil(products.length / productsPerPage)
-        setTotalPages(pages)
-        
-        setProductsOnPage(seperateToPages(productsPerPage, products, pages))
-        setLoading(false)
+        if (products.length !== 0) {
+            const pages = Math.ceil(products.length / productsPerPage)
+            setTotalPages(pages)
+            
+            setProductsOnPage(seperateToPages(productsPerPage, products, pages))
+            setLoading(false)
+        }
     }, [products])
 
     console.log('LOOK HERE', productsOnPage)
 
     return (
-        <div>
-            { products.length === 0 ? (
+        <>
+            { products.length === 0 || loading ? (
                 <div className='relative'><TruckComponentLoader loading={loading}/></div>
             ) : (
-                <div className={`h-screen grid-row-${productsPerPage} space-y-4`}>
-                    {Object.entries(productsOnPage[curPage - 1]).map(([productId, productData]) => (
-                        <div key={ productId }>
-                            <ProductCatalogItem key={ productId } productData={ productData }/>
-                        </div>
-                    ))}
-                </div>
+                <>
+                    <div className={`grid-rows-${productsPerPage} grid-rows-7 space-y-4`}>
+                        {Object.entries(productsOnPage[curPage - 1])?.map(([productId, productData]) => (
+                            <div key={ productId }>
+                                <ProductCatalogItem key={ productId } productData={ productData }/>
+                            </div>
+                        ))}
+                    </div>
+
+                    <span className='flex justify-end gap-3'>
+                        {/** First Page button */}
+                        <PaginationArrowButton
+                            onClick={() => setCurPage(1)} 
+                            disabled={curPage === 1}
+                        >
+                            <FiChevronsLeft size={24}/>
+                        </PaginationArrowButton>
+
+                        {/** Previous Page button */}
+                        <PaginationArrowButton  
+                            onClick={() => setCurPage(curPage - 1)}
+                            disabled={curPage === 1} 
+                        >
+                            <FaChevronLeft size={16}/>
+                        </PaginationArrowButton>
+
+                        <PaginationNumberButtons 
+                            totalPages={totalPages} 
+                            page={curPage} 
+                            setPage={setCurPage}
+                        />
+
+                        {/** Next Page button */}
+                        <PaginationArrowButton 
+                            onClick={() => setCurPage(curPage + 1)} 
+                            disabled={curPage === totalPages}
+                        >
+                            <FaChevronRight size={16}/>
+                        </PaginationArrowButton>
+
+                        {/** Last Page button */}
+                        <PaginationArrowButton
+                            onClick={() => setCurPage(totalPages)} 
+                            disabled={curPage === totalPages}
+                        >
+                            <FiChevronsRight size={24}/>
+                        </PaginationArrowButton>
+                    </span>
+                </>
             )}
 
-            <span className='flex justify-end gap-3'>
-                {/** First Page button */}
-                <PaginationArrowButton
-                    onClick={() => setCurPage(1)} 
-                    disabled={curPage === 1}
-                >
-                    <FiChevronsLeft size={24}/>
-                </PaginationArrowButton>
-
-                {/** Previous Page button */}
-                <PaginationArrowButton  
-                    onClick={() => setCurPage(curPage - 1)}
-                    disabled={curPage === 1} 
-                >
-                    <FaChevronLeft size={16}/>
-                </PaginationArrowButton>
-
-                <PaginationNumberButtons 
-                    totalPages={totalPages} 
-                    page={curPage} 
-                    setPage={setCurPage}
-                />
-
-                {/** Next Page button */}
-                <PaginationArrowButton 
-                    onClick={() => setCurPage(curPage + 1)} 
-                    disabled={curPage === totalPages}
-                >
-                    <FaChevronRight size={16}/>
-                </PaginationArrowButton>
-
-                {/** Last Page button */}
-                <PaginationArrowButton
-                    onClick={() => setCurPage(totalPages)} 
-                    disabled={curPage === totalPages}
-                >
-                    <FiChevronsRight size={24}/>
-                </PaginationArrowButton>
-            </span>
-        </div>
+            
+        </>
     )
 }
 
