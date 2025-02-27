d5f1e11aec6f07f934902c7e5388a0fd1cb1707c
Jin <ajhz011@aucklanduni.ac.nz>
2025-02-27 16:22:56 +1300

    Adjusted OrderStatusSelector to use OrderStatus and delegated the color changing to OrderStatus

--- ecosphere-goods/src/components/pages/dashboard/orders-page/OrderStatus.js
+++ ecosphere-goods/src/components/pages/dashboard/orders-page/OrderStatus.js
@@ -2,12 +2,27 @@ import React from 'react'
 import { FaCircle } from "react-icons/fa";
 
 const OrderStatus = ({ orderStatus }) => {
-  const isPending = orderStatus === 'Pending'
+  let orderStatusColor;
+
+  switch(orderStatus) {
+    case 'Cancelled':
+      orderStatusColor = 'bg-red-500 text-red-600 outline-red-500';
+      break;
+    case 'Pending':
+      orderStatusColor = 'bg-orange-400 text-orange-600 outline-orange-500';
+      break;
+    case 'Delivered':
+      orderStatusColor = 'bg-green-400 text-green-700 outline-green-600';
+      break;
+    default:
+      orderStatusColor = 'bg-dark-brown text-off-white outline-dark-brown';
+      break;
+  }
 
   return (
-    <div className='flex items-center space-x-3 p-2 pl-4 pr-4 rounded-full bg-orange-300 bg-opacity-30'>
-        <FaCircle className={`${isPending ? 'text-orange-500' : 'text-red-600'}`}/>
-        <p className={`font-header ${isPending ? 'text-orange-500' : 'text-red-600'}`}>{orderStatus}</p>
+    <div className={`flex items-center space-x-3 p-2 pl-4 pr-4 rounded-full bg-opacity-30 ${orderStatusColor} hover:outline`}>
+      <FaCircle/>
+      <p className='font-header'>{orderStatus}</p>
     </div>
   )
 }
--- ecosphere-goods/src/components/utility/selectors/OrderStatusSelector.js
+++ ecosphere-goods/src/components/utility/selectors/OrderStatusSelector.js
@@ -1,27 +1,11 @@
 import React, { useState } from 'react';
 import { FaCircle } from "react-icons/fa";
+import OrderStatus from '../../pages/dashboard/orders-page/OrderStatus';
 
 export default function OrderStatusSelector({ initialStatus }) {
   const [orderStatus, setOrderStatus] = useState(initialStatus);
   const [open, setOpen] = useState(false)
   const statusOptions = ['Cancelled', 'Pending', 'Delivered'];
-
-  let orderStatusColor;
-
-  switch(orderStatus) {
-    case 'Cancelled':
-      orderStatusColor = 'bg-red-500 text-red-600 outline-red-500';
-      break;
-    case 'Pending':
-      orderStatusColor = 'bg-orange-300 text-orange-600 outline-orange-400';
-      break;
-    case 'Delivered':
-      orderStatusColor = 'bg-green-400 text-green-700 outline-green-600';
-      break;
-    default:
-      orderStatusColor = 'bg-dark-brown text-off-white outline-dark-brown';
-      break;
-  }
 
   const handleSelect = (option) => {
     setOrderStatus(option);
@@ -36,13 +20,9 @@ export default function OrderStatusSelector({ initialStatus }) {
       />
 
       {/** Displaying the status */}
-      <div 
-        className={`flex p-2 pl-4 pr-4 rounded-full bg-opacity-30 border-none justify-center items-center gap-3 font-header w-full ${orderStatusColor} hover:outline outline-3`}
-        onClick={() => setOpen(true)}
-      >
-        <FaCircle />
-        {orderStatus}
-      </div>
+      <button onClick={() => setOpen(true)} className='w-full'>
+        <OrderStatus orderStatus={orderStatus}/>
+      </button>
 
       {/** Status options */}
       <div className={`flex flex-col absolute z-10 items-center justify-center bg-off-white rounded-lg w-32 mt-2 overflow-hidden opacity-0 ${ open ? 'opacity-100 scale-100' : 'pointer-events-none scale-90'} transition-all ease-in-out duration-100`}>
