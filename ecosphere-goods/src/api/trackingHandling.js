8e9dc246295476177c4cd455f7e62be07429fba1
Jin <ajhz011@aucklanduni.ac.nz>
2025-03-05 19:45:52 +1300

    Adjusted tackingHandling to properly use the trackingNumber parameter

--- ecosphere-goods/src/api/trackingHandling.js
+++ ecosphere-goods/src/api/trackingHandling.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const fetchTrackingInfo = async (trackingNumber) => {
     try {
-        const res = axios.get(`/package-tracking/package-info/TEST1234123421`) 
+        const res = axios.get(`/package-tracking/package-info/${trackingNumber}`) 
 
         return res
     } catch (err) {
