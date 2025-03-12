6b5ff81b659b971417d22dae38aa4919253e7c50
Jin <ajhz011@aucklanduni.ac.nz>
2025-03-12 15:08:56 +1300

    Created mod data for getPackageLocation rather than using fetchPackageLocation since the free trial of the API ran out

--- backend/controllers/packageTrackingController.js
+++ backend/controllers/packageTrackingController.js
@@ -34,9 +34,71 @@ const getPackageLocation = async (req, res) => {
     const { trackingNumber } = req.params
 
     try {
-        const trackingRes = await fetchPackageLocation(trackingNumber);
+        // const trackingRes = await fetchPackageLocation(trackingNumber);
 
-        return res.status(201).json({ data: trackingRes })
+        const mockData = {
+            originTrackInfo: [
+                {
+                    location: "New York - US",
+                    checkpoint_delivery_status: "Order Recieved",
+                    checkpoint_date: "2025-01-3T12:00",
+                    tracking_detail: "Package order received"
+                },
+                {
+                    location: "New York - US",
+                    checkpoint_delivery_status: "Order Recieved",
+                    checkpoint_date: "2025-01-3T12:00",
+                    tracking_detail: "Package order received"
+                },
+                {
+                    location: "New York - US",
+                    checkpoint_delivery_status: "In Transit",
+                    checkpoint_date: "2025-01-4T16:34",
+                    tracking_detail: "Package order received"
+                },
+                {
+                    location: "New York - US",
+                    checkpoint_delivery_status: "In Transit",
+                    checkpoint_date: "2025-01-5T13:23",
+                    tracking_detail: "Package order received"
+                },
+                {
+                    location: "New York - US",
+                    checkpoint_delivery_status: "Shipping",
+                    checkpoint_date: "2025-01-5T08:23",
+                    tracking_detail: "Package order received"
+                }
+            ],
+            destinationTrackInfo: [
+                {
+                    location: "Auckland - NZ",
+                    checkpoint_delivery_status: "In Customs",
+                    checkpoint_date: "2025-01-10T04:45",
+                    tracking_detail: "Package order received"
+                },
+                {
+                    location: "Auckland - NZ",
+                    checkpoint_delivery_status: "Processing",
+                    checkpoint_date: "2025-01-11T14:17",
+                    tracking_detail: "Package order received"
+                },
+                {
+                    location: "Auckland - NZ",
+                    checkpoint_delivery_status: "On the Way",
+                    checkpoint_date: "2025-01-12T13:23",
+                    tracking_detail: "Package order received"
+                },
+                {
+                    location: "Auckland - NZ",
+                    checkpoint_delivery_status: "Delivered",
+                    checkpoint_date: "2025-01-12T17:01",
+                    tracking_detail: "Package order received"
+                }
+            ]
+            
+        }
+
+        return res.status(201).json({ data: mockData })
     } catch (err) {
         console.error(err.message)
         return res.status(500).json({ message: err.message })
