9d73cc44ec4ba70a310335cc2f212614e7b17515
Jin <ajhz011@aucklanduni.ac.nz>
2025-03-22 16:31:06 +1300

    Adjusted firebase admin sdk structure

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -2,8 +2,7 @@ const admin = require('firebase-admin')
 const path = require('path')
 require('dotenv').config()
 
-const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH;
-const serviceAccount = require(path.join(__dirname, serviceAccountPath))
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK)
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
